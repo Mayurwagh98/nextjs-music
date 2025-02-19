baeb14e1127da61940556fa1983615fbf4c25095
mayur wagh <mswagh98@gmail.com>
2025-02-19 21:56:30 +0530

    Enhances button styling and adds shimmer animation
    Updates the button in the Featured component to improve its visual appeal with a new gradient background and hover effects.
    
    Adds a shimmer animation to the Tailwind CSS configuration for a modern loading effect.
    
    This change enhances user experience by making the interface more engaging.

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -58,8 +58,8 @@ const Featured = () => {
       </div>
 
       <div className="mt-20 text-center">
-        <button className="bg-purple-500 text-white px-3 py-2 rounded-md">
-          View courses
+        <button className="text-white inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-7 font-medium transition-colors focus:outline-none">
+          View All
         </button>
       </div>
     </div>

065d67cdcf9951fc37df3d48443fa8f509b7a902
mayur wagh <mswagh98@gmail.com>
2025-02-20 21:07:31 +0530

    Adds Upcoming Lessons component to homepage
    Introduces the Upcoming Lessons component to display scheduled music lessons and workshops.
    
    Includes a new card hover effect for an enhanced user experience when interacting with lesson details.
    
    This feature aims to provide users with easy access to live lesson information and improve engagement.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Featured from "@/components/Featured";
 import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
+import UpComingLessons from "@/components/UpcomingLessons";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Featured />
       <Testimonials />
+      <UpComingLessons />
     </main>
   );
 }
